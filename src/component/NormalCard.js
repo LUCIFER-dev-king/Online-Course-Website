5d64e425ca5bdb8c479b57d08d3c0ce3dceb076c
LUCIFER-dev-king <nihaljesiya@gmail.com>
2022-04-04 16:54:25 +0530

    cart card alignment adjusted

--- src/component/NormalCard.js
+++ src/component/NormalCard.js
@@ -47,7 +47,7 @@ const NormalCard = ({ course, fromCart, user, setCartList, cartList }) => {
         ></img>
       </div>
 
-      <div className="card-body p-0 ps-2 py-2">
+      <div className="card-body p-0 px-2 py-2">
         <h5 className="card-title fw-bolder m-0">{courseName}</h5>
         <p className="card-text text-muted">{authorName}</p>
         <div className="d-flex align-items-center">
@@ -75,7 +75,7 @@ const NormalCard = ({ course, fromCart, user, setCartList, cartList }) => {
               removeItemHandler();
             }}
             style={{ cursor: "pointer" }}
-            className="rounded bg-dark mt-1 text-center w-100 text-light px-4 py-2 fw-bold"
+            className="rounded bg-dark my-1 w-100 text-center text-light px-4 py-2 fw-bold"
           >
             Remove from cart
           </div>
